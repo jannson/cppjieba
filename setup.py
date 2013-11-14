ed4b46cccdbdb619eedd18b5e0bb4ed0ad993555
janson <janson@localhost.localdomain>
2013-11-15 02:22:24 +0800

    modify for x64

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+import os
 from distutils.core import setup, Extension
 
-module_jieba = Extension('cppjiebapy/_mixsegment',
-                ['cppjiebapy/mixsegment.i','cppjiebapy/mixsegment.cpp'],
+ignores = ['server.cpp','segment.cpp']
+sources = ['cppjiebapy/mixsegment.i','cppjiebapy/mixsegment.cpp']
+paths = ['./src', './src/Limonp', './src/Husky']
+
+for p in paths:
+    for d in os.listdir(p):
+        f = os.path.join(p, d)
+        if os.path.isfile(f) and d.endswith(".cpp") and d != 'server.cpp':
+            sources.append(f)
+
+module_jieba = Extension('_mixsegment',
+                sources,
                 language='c++',
                 swig_opts=['-c++'],
                 #define_macros = [('TEST', 1)],
                 #extra_compile_args=['-static'],
                 #extra_link_args=['-static'],
-                include_dirs=['/usr/include/CppJieba', 'cppjieba'],
-                libraries=['cppjieba'],
-                library_dirs=['/usr/lib/CppJieba'])
+                #include_dirs=['/usr/include/CppJieba', 'cppjieba'],
+                #libraries=['cppjieba'],
+                #library_dirs=['/usr/lib/CppJieba']
+                )
 
 setup(name='cppjiebapy',
         version='0.1.0',
